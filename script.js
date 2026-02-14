198191c202ad69e0f1df25c5fc32148ddf7d2f57
Dongjean <119925419+Dongjean@users.noreply.github.com>
2026-02-14 22:41:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+const words = ["ahple", "grapw", "mimik", "traim", "bruhs", "planr"];
+const solution = words[Math.floor(Math.random() * words.length)];
+
+const board = document.getElementById("board");
+const message = document.getElementById("message");
+const buttonCont = document.getElementById("buttonCont");
+
+let currentRow = 0;
+let currentGuess = "";
+let gameOver = false;
+
+const totalGuesses = 6;
+
+let tries = 2;
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Create board
+createBoard();
+function createBoard() {
+  for (let i = 0; i < totalGuesses; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < 5; j++) {
+      const tile = document.createElement("div");
+      tile.classList.add("tile");
+      row.appendChild(tile);
+    }
+    board.appendChild(row);
+  }
+}
+
+function revealGuess() {
+  const row = board.children[currentRow];
+  const solutionLetters = solution.split("");
+
+  for (let i = 0; i < 5; i++) {
+    const tile = row.children[i];
+    const letter = currentGuess[i];
+
+    setTimeout(() => {
+      tile.classList.add("flip");
+
+      if (letter === solution[i]) {
+        tile.classList.add("correct");
+        solutionLetters[i] = null;
+      } else if (solutionLetters.includes(letter)) {
+        tile.classList.add("present");
+        solutionLetters[solutionLetters.indexOf(letter)] = null;
+      } else {
+        tile.classList.add("absent");
+      }
+    }, i * 300);
+  }
+
+  setTimeout(() => {
+    if (currentGuess === solution) {
+      message.textContent = "You Win!";
+      gameOver = true;
+      return;
+    }
+
+    currentRow++;
+    currentGuess = "";
+
+    if (currentRow === totalGuesses) {
+      tries -= 1;
+      message.textContent = "Game Over! You have " + tries + " tries left...";
+      if (tries == 0) {
+        gameOver = true;
+        nextPage();
+      } else {
+        buttonCont.children[0].textContent = "Try Again";
+        buttonCont.children[0].addEventListener('click', () => {
+          buttonCont.children[0].textContent = null;
+          message.textContent = null;
+
+          board.innerHTML = '';
+          currentGuess = "";
+          currentRow = 0;
+
+          createBoard();
+        })
+      }
+    }
+  }, 1600);
+}
+
+async function nextPage() {
+
+  // buttonCont.children[0].textContent = "⇨";
+  // buttonCont.children[0].addEventListener('click', () => {
+  //   window.location.href = 'main.html';
+  // })
+
+  if (confirm("Wait, you've got some new mail!")) {
+    window.location.href = 'main.html';
+  }
+
+  return;
+}
+
+function handleKey(e) {
+  if (gameOver) return;
+
+  if (tries == 1 && currentRow == 3 && currentGuess.length == 3) {
+    nextPage();
+
+    return;
+  }
+  const row = board.children[currentRow];
+
+  if (e.key === "Backspace") {
+    currentGuess = currentGuess.slice(0, -1);
+  } else if (e.key === "Enter") {
+    if (currentGuess.length !== 5) {
+      row.classList.add("shake");
+      setTimeout(() => row.classList.remove("shake"), 400);
+      return;
+    }
+    revealGuess();
+    return;
+  } else if (/^[a-zA-Z]$/.test(e.key) && currentGuess.length < 5) {
+    currentGuess += e.key.toLowerCase();
+    const tile = row.children[currentGuess.length - 1];
+    tile.classList.add("pop");
+    setTimeout(() => tile.classList.remove("pop"), 100);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    row.children[i].textContent = currentGuess[i] || "";
+  }
+}
+
+document.addEventListener("keydown", handleKey);
---
+++ script2.js
@@ -0,0 +1,169 @@
+const paragraph = `Dear Claire,
+
+Happy Valentine's Day babygirl! Even now, I cant believe how lucky I am to be able to call you mine. Honestly, if I had to do it all over again, It'd always be you. We may have our ups and downs, and we may not be the most perfect couple, but I love you, in all of your perfect imperfections that make you human, my human.
+
+Really dont know how you do it, but your existence just reels me in. You're my first thought in the morning, and my last thought I cherish to bed. You're the one who gives me pep talks in my head before exams, and you're the one who gives me strength to push through hard times. You're my girl, and I'm yours, and no matter what the world throws at us, I'll always stand by you.
+
+Thank you for loving me. Thank you for always showing up even when things are hard and stressful, and I pledge to return this lifelong debt that I'm not sure how I can repay. I love you truly, and cheers to 1000 more years of love!
+
+With love,
+Doji <3
+
+P.S.
+anyway...`;
+
+// const paragraph = "hello bruh wtf";
+
+const letterCont = document.getElementsByClassName("letterCont");
+const buttonCont = document.getElementsByClassName("buttonCont");
+const openMailButton = document.getElementsByClassName("mailButton");
+const mailNotif = document.getElementById("mailNotif");
+
+const requestText = document.getElementsByClassName("request");
+
+const letterContent = letterCont[0].children[0];
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function autoType() {
+
+  // Turn on the cursor
+  // console.log(letterCont[0].children[0].children[1]);
+  letterCont[0].children[0].children[1].innerHTML = '|';
+  await delay(1);
+  for (let i = 0; i<paragraph.length; i++) {
+    const currChar = paragraph[i];
+    if (currChar == ' ') {
+      
+      // Only do the random pauses between words
+      const isPause = Math.floor(Math.random() * 10);
+      if (isPause > 9) {
+        await delay(2000);
+      }
+    }
+
+    const typingSpeed = Math.floor(Math.random() * 10) * 10 + 50;
+    await delay(typingSpeed);
+    
+    letterCont[0].children[0].children[0].innerHTML += currChar;
+  }
+
+  // After we are done, start the request
+  askher();
+}
+
+function askher() {
+  requestText[0].classList.add("ask");
+}
+
+function openOptions() {
+  buttonCont[0].classList.add('ask');
+
+  const noBox = buttonCont[0].children[1];
+  document.addEventListener("mousemove", (e) => {
+
+    // Get box coords
+    const rect = noBox.getBoundingClientRect();
+    const boxX = rect.left + rect.width / 2;
+    const boxY = rect.top + rect.height / 2;
+
+    // Get distance of mouse from box
+    const dx = e.clientX - boxX;
+    const dy = e.clientY - boxY;
+    const distance = Math.sqrt(dx*dx + dy*dy);
+
+    // How far it moves visually
+    const repelDistance = 80;
+
+    if (distance < 150) {
+      const angle = Math.atan2(dy, dx);
+      const moveX = -Math.cos(angle) * repelDistance;
+      const moveY = -Math.sin(angle) * repelDistance;
+
+      noBox.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    } else {
+      noBox.style.transform = `translate(0, 0)`; // Return to normal
+    }
+  });
+}
+
+// Add in animation for yes 존나 뿅뿅
+function yes() {
+
+  const heartCont = document.getElementById("heartCont");
+
+  document.body.style.backgroundColor = "#FF7782";
+
+  for (let j = 0; j < 500; j++) {
+    const heart = document.createElement("img");
+    heart.src = "P03fav.png";
+    heart.classList.add("heart");
+
+    const angle = Math.random() * 2 * Math.PI;
+    const distance = 1000 + Math.random() * 200;
+    const x = Math.cos(angle) * distance;
+    const y = Math.sin(angle) * distance;
+
+    const duration = 1000 + Math.random() * 1000;
+
+    heart.style.transform = `translate(0,0)`;
+    heart.style.opacity = 1;
+
+    heartCont.appendChild(heart);
+    // Hearts are added
+
+    // Now animate yayz
+    heart.animate([
+      { transform: `translate(0,0) scale(1)`, opacity: 1 },
+      { transform: `translate(${x}px, ${-y}px) scale(0.5)`, opacity: 0 }
+    ], {
+      duration: duration,
+      easing: "cubic-bezier(.17,.67,.83,.67)"
+    });
+
+    setTimeout(() => {
+      heart.remove();
+    }, duration);
+
+  }
+  //setInterval(emojiwaterfall(), 200);
+}
+
+function emojiwaterfall() {
+  const emojils = ["✨", "💖", "🔥", "🌸", "⭐", "💫"];
+  const emoji = document.createElement("div");
+  emoji.classList.add("emoji");
+
+  emoji.textContent = emojils[Math.floor(Math.random() * emojils.length)];
+
+  emoji.style.left = Math.random() * 100 + "vw";
+  emoji.style.fontSize = Math.random() * 20 + 15 + "px";
+  emoji.style.animationDuration = Math.random() * 3 + 3 + "s";
+
+  heartCont.appendChild(emoji);
+
+  setTimeout(() => {
+    emoji.remove();
+  }, 6000);
+}
+
+requestText[0].addEventListener('transitionend', () => {
+  openOptions();
+});
+
+openMailButton[0].addEventListener('click', () => {
+  openMailButton[0].classList.add("animate");
+  mailNotif.remove();
+});
+
+openMailButton[0].addEventListener('transitionend', () => {
+  openMailButton[0].remove();
+  autoType();
+})
+
+const yesButton = buttonCont[0].children[0];
+yesButton.addEventListener('click', () => {
+  yes();
+})
